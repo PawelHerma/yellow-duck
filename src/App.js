c03d65938004e3e90788c3420845ae165736176b
PawelHerma <pawel.slawomir.herma@gmail.com>
2023-04-16 05:32:46 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+const responses = [
+  "Rozumiem",
+  "Okej, w jaki sposób chciałbyś/chciałabyś to zrobić?",
+  "Co dalej?",
+  "W jaki sposób masz zamiar to osiągnąć?",
+  "Jaki masz na to pomysł?",
+  "To jest dobry pomysł",
+  "Jak chcesz to rozwinąć?",
+];
 
 function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const handleInput = (event) => {
+    setInput(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    const response =
+      responses[Math.floor(Math.random() * responses.length)];
+
+    setMessages([
+      ...messages,
+      { message: input, user: true },
+      { message: response, user: false },
+    ]);
+
+    setInput("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="chat">
+        {messages.map((message, index) => (
+          <div
+            key={index}
+            className={`message ${message.user ? "user" : "bot"}`}
+          >
+            <div className="message-content">{message.message}</div>
+          </div>
+        ))}
+        <div className="input">
+          <input value={input} onChange={handleInput} />
+          <button onClick={handleSubmit}>Send</button>
+        </div>
+      </div>
     </div>
   );
 }
